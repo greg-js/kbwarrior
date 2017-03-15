cb6f7a86ed5ab5912b5fd7ba31149d047e837ac0
Gregory Scheerlinck <gregory.scheerlinck@gmail.com>
2017-03-15 08:03:50 +0000

    blacklist menu first pass

--- popup.js
+++ popup.js
@@ -1,32 +1,43 @@
-var opened = false;
+var instructionsOpen = false;
+var blacklistOpen = false;
 
 document.addEventListener('DOMContentLoaded', function() {
   var html = document.querySelector('html');
   var body = document.body;
   var buttonInstr = document.querySelector('.kbw-instructions');
+  var buttonBlacklist = document.querySelector('.kbw-blacklist');
   var buttonProj = document.querySelector('.kbw-project-home');
   var buttonBlog = document.querySelector('.kbw-gregjs');
   var tableDiv = document.getElementById('table-div');
+  var listDiv = document.getElementById('blacklist-div');
 
-  var closedHeight = buttonBlog.getBoundingClientRect().bottom + 10 + 'px';
-  var openedHeight = tableDiv.getBoundingClientRect().bottom + 10 + 'px';
+  var heightClosed = buttonBlog.getBoundingClientRect().bottom + 10;
+  var heightInstrOpen = heightClosed + tableDiv.offsetHeight + 10;
+  var heightListOpen = heightClosed + listDiv.offsetHeight + 10;
 
-  body.style.height = closedHeight;
-  html.style.height = closedHeight;
+  body.style.height = heightClosed;
+  html.style.height = heightClosed;
   body.style.width = '190px';
   html.style.width = '190px';
 
+  buttonBlacklist.addEventListener('click', function() {
+    makeVisible(
+      blacklistOpen ? heightClosed : heightListOpen,
+      listDiv,
+      instructionsOpen ? tableDiv : null
+    );
+    instructionsOpen = false;
+    blacklistOpen = !blacklistOpen;
+  });
+
   buttonInstr.addEventListener('click', function() {
-    if (!opened) {
-      body.style.height = openedHeight;
-      html.style.height = openedHeight;
-      body.style.width = '190px';
-      html.style.width = '190px';
-    } else {
-      html.style.height = closedHeight;
-      body.style.height = closedHeight;
-    }
-    opened = !opened;
+    makeVisible(
+      instructionsOpen ? heightClosed : heightInstrOpen,
+      tableDiv,
+      blacklistOpen ? listDiv : null
+    );
+    blacklistOpen = false;
+    instructionsOpen = !instructionsOpen;
   });
 
   buttonProj.addEventListener('click', function() {
@@ -40,4 +51,13 @@ document.addEventListener('DOMContentLoaded', function() {
       url: 'https://www.gregjs.com'
     });
   });
+
+  function makeVisible(height, element, old) {
+    if (old) old.style.display = 'none';
+    element.style.display = 'block';
+    body.style.height = height + 'px';
+    html.style.height = height + 'px';
+    body.style.width = '190px';
+    html.style.width = '190px';
+  }
 });
